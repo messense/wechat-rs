10538bdeee2e1678003c48c7f0922c0162ab60dd
messense <messense@icloud.com>
2015-06-03 22:41:07 +0800

    Update message parser

--- src/parser.rs
+++ src/parser.rs
@@ -11,6 +11,9 @@ pub fn parse_message(xml: &str) -> Message {
     let msg_type = &msg_type_str[..];
     let msg = match msg_type {
         "text" => Message::Text(messages::TextMessage::from_xml(xml)),
+        "image" => Message::Image(messages::ImageMessage::from_xml(xml)),
+        "voice" => Message::Voice(messages::VoiceMessage::from_xml(xml)),
+        "shortvideo" => Message::ShortVideo(messages::ShortVideoMessage::from_xml(xml)),
         _ => Message::Unknown(messages::UnknownMessage::from_xml(xml)),
     };
     msg

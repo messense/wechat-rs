c1b93b9b61448974a2f123e3e38df42faca34a58
messense <messense@icloud.com>
2015-06-16 23:27:10 +0800

    Add ImageReply

--- src/replies/mod.rs
+++ src/replies/mod.rs
@@ -3,9 +3,12 @@ pub trait ReplyRenderer {
 }
 
 mod text;
+mod image;
 
 pub use self::text::TextReply;
+pub use self::image::ImageReply;
 
 pub enum Reply {
     TextReply(TextReply),
+    ImageReply(ImageReply),
 }

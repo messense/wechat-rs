e2b8d73c45a3614c6d891aaa6f2bcf46fdd0d833
messense <messense@icloud.com>
2015-10-16 11:33:20 +0800

    Update menu APIs to use Encodable instead of Json

--- src/client/api/menu.rs
+++ src/client/api/menu.rs
@@ -1,3 +1,4 @@
+use rustc_serialize::Encodable;
 use rustc_serialize::json::Json;
 
 use client::WeChatClient;
@@ -17,19 +18,21 @@ impl<'a> WeChatMenu<'a> {
         }
     }
 
-    pub fn create(&self, data: &Json) -> Result<Json, WeChatError> {
-        self.client.post("menu/create", vec![], data.as_object().unwrap())
+    pub fn create<D: Encodable>(&self, data: &D) -> Result<(), WeChatError> {
+        try!(self.client.post("menu/create", vec![], data));
+        Ok(())
     }
 
     pub fn get(&self) -> Result<Json, WeChatError> {
         self.client.get("menu/get", vec![])
     }
 
-    pub fn delete(&self) -> Result<Json, WeChatError> {
-        self.client.get("menu/delete", vec![])
+    pub fn delete(&self) -> Result<(), WeChatError> {
+        try!(self.client.get("menu/delete", vec![]));
+        Ok(())
     }
 
-    pub fn update(&self, data: &Json) -> Result<Json, WeChatError> {
+    pub fn update<D: Encodable>(&self, data: &D) -> Result<(), WeChatError> {
         try!(self.delete());
         self.create(data)
     }

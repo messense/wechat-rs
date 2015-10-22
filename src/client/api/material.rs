1e9c0c3818800971eb197b861881b70d6a3f358f
messense <messense@icloud.com>
2015-10-22 23:52:56 +0800

    Add more material API. [ci skip]

--- src/client/api/material.rs
+++ src/client/api/material.rs
@@ -5,7 +5,7 @@ use types::WeChatResult;
 use client::WeChatClient;
 use session::SessionStore;
 use client::request::ArticleMaterial;
-use client::response::Material;
+use client::response::{Material, MaterialCount};
 
 
 #[derive(Debug, Clone)]
@@ -44,6 +44,40 @@ impl<'a, T: SessionStore> WeChatMaterial<'a, T> {
         Ok(Material {
             media_id: media_id.to_owned(),
             url: None,
+        })
+    }
+
+    pub fn update_article<S: AsRef<str>>(&self, media_id: S, index: usize, article: ArticleMaterial) -> WeChatResult<()> {
+        let data = json!({
+            "media_id": (media_id.as_ref()),
+            "index": (index),
+            "articles": (article)
+        });
+        try!(self.client.post("material/update_news", vec![], &data));
+        Ok(())
+    }
+
+    pub fn delete<S: AsRef<str>>(&self, media_id: S) -> WeChatResult<()> {
+        let data = json!({"media_id": (media_id.as_ref())});
+        try!(self.client.post("material/del_material", vec![], &data));
+        Ok(())
+    }
+
+    pub fn get_count(&self) -> WeChatResult<MaterialCount> {
+        let res = try!(self.client.get("material/get_materialcount", vec![]));
+        let voice_count = &res["voice_count"];
+        let voice_count = voice_count.as_u64().unwrap();
+        let video_count = &res["video_count"];
+        let video_count = video_count.as_u64().unwrap();
+        let image_count = &res["image_count"];
+        let image_count = image_count.as_u64().unwrap();
+        let articles_count = &res["news_count"];
+        let articles_count = articles_count.as_u64().unwrap();
+        Ok(MaterialCount {
+            voice_count: voice_count as usize,
+            video_count: video_count as usize,
+            image_count: image_count as usize,
+            articles_count: articles_count as usize,
         })
     }
 }
--- src/client/response/material.rs
+++ src/client/response/material.rs
@@ -3,3 +3,11 @@ pub struct Material {
     pub media_id: String,
     pub url: Option<String>,
 }
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct MaterialCount {
+    pub voice_count: usize,
+    pub video_count: usize,
+    pub image_count: usize,
+    pub articles_count: usize,
+}
